344c882e42eb22b845dc95bdd01d22e2bc218229
uytaerkjoal-cpu <uytaerkjoal@gmail.com>
2026-05-21 00:10:02 +0600

    Update start.bat

--- Anux/start.bat
+++ Anux/start.bat
@@ -1,1 +1,31 @@
+@echo off
+set VENV_DIR=venv
 
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    py --version >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Python not found
+        pause
+        exit /b
+    ) else (
+        set PYTHON_CMD=py
+    )
+) else (
+    set PYTHON_CMD=python
+)
+
+if not exist %VENV_DIR% (
+    echo Venv...
+    %PYTHON_CMD% -m venv %VENV_DIR%
+)
+
+call %VENV_DIR%\Scripts\activate.bat
+
+echo Pip...
+python -m pip install --upgrade pip --quiet
+pip install ultralytics torch torchvision numpy opencv-python pygame mss pywin32 --quiet
+echo Launch...
+python ai.py
+
+pause
