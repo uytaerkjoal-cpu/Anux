de9f79dee11f5dffca2b835077f95aa724811471
uytaerkjoal-cpu <uytaerkjoal@gmail.com>
2026-05-21 13:43:12 +0600

    Update start.bat

--- Anux/start.bat
+++ Anux/start.bat
@@ -1,5 +1,6 @@
 @echo off
 set VENV_DIR=venv
+set PYTHON_PATH=%VENV_DIR%\Scripts\python.exe
 
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
@@ -9,23 +10,22 @@ if %errorlevel% neq 0 (
         pause
         exit /b
     ) else (
-        set PYTHON_CMD=py
+        set BASE_PYTHON=py
     )
 ) else (
-    set PYTHON_CMD=python
+    set BASE_PYTHON=python
 )
 
 if not exist %VENV_DIR% (
-    echo Venv...
-    %PYTHON_CMD% -m venv %VENV_DIR%
+    echo Creating Venv...
+    %BASE_PYTHON% -m venv %VENV_DIR%
 )
 
-call %VENV_DIR%\Scripts\activate.bat
+echo Installing requirements...
+%PYTHON_PATH% -m pip install --upgrade pip --quiet
+%PYTHON_PATH% -m pip install ultralytics torch torchvision numpy opencv-python pygame mss pywin32 --quiet
 
-echo Pip...
-python -m pip install --upgrade pip --quiet
-pip install ultralytics torch torchvision numpy opencv-python pygame mss pywin32 --quiet
-echo Launch...
-python ai.py
+echo Launching...
+%PYTHON_PATH% ai.py
 
 pause
